f1697625cfd1e9178e3d6d4044903dad592ede13
PokeGuys <5060799+PokeGuys@users.noreply.github.com>
2024-08-04 01:24:21 +0800

    feat: rename apkpure response field

--- apk.go
+++ apk.go
@@ -1,10 +1,10 @@
 package apkcrawler
 
 type Apk struct {
-	Name      string
-	Version   string
-	Size      int64
-	URL       string
-	Hash      string
-	CreatedAt int64
+	Name    string
+	Package string
+	Version string
+	Size    int64
+	URL     string
+	Hash    string
 }
