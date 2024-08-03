c092f02c1e3b17dcd7faec247b4552ec8fa5c0c9
PokeGuys <5060799+PokeGuys@users.noreply.github.com>
2024-08-03 23:11:15 +0800

    feat: add apkpure as crawling source

---
+++ cmd/cralwer/crawler.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/go-resty/resty/v2"
+
+	"github.com/pokeguys/apk-crawler/cmd/cralwer/config"
+	"github.com/pokeguys/apk-crawler/sources"
+)
+
+func main() {
+	cfg := config.NewConfig()
+
+	// Setup the http client for the crawler
+	client := resty.New()
+	crawler := sources.NewCrawlerStrategy(cfg.Source, client)
+
+	// Crawl the package
+	apk, err := crawler.Crawl(cfg.Package)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	// Print the results
+	// If the user pass showAll flag, print all the results
+	if cfg.ShowAll {
+		for _, a := range apk {
+			fmt.Println(a.URL)
+		}
+	} else {
+		fmt.Println(apk[0].URL)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/pokeguys/apk-crawler
 
 go 1.22.4
+
+require (
+	github.com/go-resty/resty/v2 v2.11.0
+	google.golang.org/protobuf v1.34.2
+)
+
+require golang.org/x/net v0.27.0 // indirect
